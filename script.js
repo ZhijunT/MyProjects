ff07ff5230117908f7ad259b777ef64dd44308b3
Zhijun Tian <zht226@lehigh.edu>
2025-05-26 19:25:10 -0400

    Delete all files

--- script.js
+++
@@ -1,5 +0,0 @@
-function changeBackground() {
-  const colors = ["#f0f0f0", "#c3f0ca", "#f9c0c0", "#c0ddf9"];
-  document.body.style.backgroundColor =
-    colors[Math.floor(Math.random() * colors.length)];
-}
